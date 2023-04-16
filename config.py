47d5f27017172960f9e32bdef5a4db483df8ca50
tkfmokmt <tkfmokmt1120@gmail.com>
2023-04-17 06:24:32 +0900

    delete unneeded file/code

--- config.py
+++ config.py
@@ -4,9 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MESSAGE_HISTORY_LIMIT = 10
-JSON_FILE_NAME = "channel_id.json"
-JSON_KEY_CHANNEL_ID = "channel_id"
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_TOKEN")
 OPEN_AI_KEY = os.getenv("OPENAI_API_KEY")
 AI_MODEL = "gpt-3.5-turbo"

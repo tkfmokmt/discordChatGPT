b88e7af177170fc6720d68cfbe0a84f1e0963d6e
tkfmokmt <tkfmokmt1120@gmail.com>
2023-04-17 06:32:35 +0900

    formatting

--- discordbot.py
+++ discordbot.py
@@ -72,8 +72,7 @@ async def on_message(discord_message):
         is_waiting_personality_text = True
         temp_channel_id = channel_id
         await discord_message.channel.send(
-            """このチャンネルの回答の人格を変更します。
-            人格を説明する文章をこのメッセージの直後に入力してください。"""
+            """このチャンネルの回答の人格を変更します。人格を説明する文章をこのメッセージの直後に入力してください。"""
         )
         return
     if database.is_reply_channel_id(channel_id):
